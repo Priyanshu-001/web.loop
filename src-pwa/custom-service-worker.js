3c8c1986df51ebc69c46f1bcd9748b6df36927f2
Priyanshu Ahuja <26710303+Priyanshu-001@users.noreply.github.com>
2023-07-27 00:24:16 +0530

    added: caputuring req poc, API single route

---
+++ src-pwa/custom-service-worker.js
@@ -0,0 +1,21 @@
+import { registerRoute } from "workbox-routing";
+import { precacheAndRoute } from "workbox-precaching";
+caputre  =
+console.log("cookie");
+// Register a route to handle all GET requests
+registerRoute(
+  // ({ request }) => {
+  //   console.log(request);
+  //   return request.path true;
+  // },
+  "/hello",
+  // Define a custom handler to respond with 'hi'
+  async ({ event }) => {
+    console.log("HELOOO");
+    return new Response("hi", {
+      headers: { "Content-Type": "text/plain" },
+    });
+  }
+);
+
+precacheAndRoute(self.__WB_MANIFEST);
