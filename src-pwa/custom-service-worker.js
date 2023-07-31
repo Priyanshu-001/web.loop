7315a2c25702b73230b90a9f1f2bc5bac60cf707
Priyanshu Ahuja <26710303+Priyanshu-001@users.noreply.github.com>
2023-07-31 20:52:15 +0530

    WIP

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -1,21 +1,73 @@
 import { registerRoute } from "workbox-routing";
 import { precacheAndRoute } from "workbox-precaching";
-
 console.log("cookie");
+const helpers = {
+  delay: (delayInms) => {
+    return new Promise((resolve) => setTimeout(resolve, delayInms));
+  },
+  getResource: (url /* handler CB url */) => {
+    const [_, resource] = url.pathname.split("qres", 2);
+    return !!resource && resource;
+  },
+};
+
+function engine() {
+  const data = {
+    serve: {
+      "/hello": {
+        delay: 0,
+        contentType: "text/plain",
+        content: "Yo boi",
+        statusCode: 500,
+      },
+    },
+    reqHits: 0,
+  };
+  const fns = (function () {
+    function captureRoute(routeObj) {
+      data.serve = { ...data.serve, ...routeObj };
+    }
+    function isKnown(link) {
+      return Object.keys(data.serve).includes(link);
+    }
+    function val(link) {
+      return data.serve[link];
+    }
+    return { val, isKnown, captureRoute };
+  })();
+  return fns;
+}
+
+let { val, isKnown, captureRoute } = engine();
+const handlers = {
+  responseHandler: async ({ url }) => {
+    const {
+      delay,
+      content,
+      contentType,
+      statusCode: status,
+    } = val(helpers.getResource(url));
+    await helpers.delay(delay);
+    return new Response(content, {
+      status,
+      headers: { "Content-Type": contentType },
+    });
+  },
+};
 // Register a route to handle all GET requests
 registerRoute(
   // ({ request }) => {
   //   console.log(request);
   //   return request.path true;
   // },
-  "/hello",
+  ({ url }) => {
+    // console.log(url);
+    if (!url.pathname.startsWith("/qres")) return false;
+    const resource = helpers.getResource(url);
+    return isKnown(resource);
+  },
   // Define a custom handler to respond with 'hi'
-  async ({ event }) => {
-    console.log("HELOOO");
-    return new Response("hi", {
-      headers: { "Content-Type": "text/plain" },
-    });
-  }
+  handlers.responseHandler
 );
 
 precacheAndRoute(self.__WB_MANIFEST);
--- src/components/CodeEditors/ApiEditor/v0/FullEditor.vue
+++ src/components/CodeEditors/ApiEditor/v0/FullEditor.vue
@@ -3,6 +3,15 @@
     <single-editor></single-editor>
   </div>
 </template>
-<script setup>
+<script setup lang="ts">
 import singleEditor from 'src/components/CodeEditors/ApiEditor/v0/SingleRoute.vue'
+//TODO: optimize it as it is highly unoptimized sending all routes for every update
+function pushRoutes(){
+  if(!navigator.serviceWorker.controller)
+    {
+      console.error("this is wierd")
+      return;
+    }
+   navigator.serviceWorker.controller.postMessage({'dest': 'routes/loading',payaload:[{'hello':'bye'}]})
+}
 </script>
