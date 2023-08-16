51a5e75dd3668cc0534dbfd6328bb40179997204
Priyanshu Ahuja <26710303+Priyanshu-001@users.noreply.github.com>
2023-08-16 23:34:58 +0530

    WIP: API editor

--- src/components/CodeEditors/ApiEditor/v0/FullEditor.vue
+++ src/components/CodeEditors/ApiEditor/v0/FullEditor.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
-    <single-editor></single-editor>
+    {{ lRoutes }}
+    <single-editor v-for=" (singleRoute,i) in allRoutes" v-model="allRoutes[i]" @update:modelValue="console.log" :key="i" />
   </div>
 </template>
 <script setup lang="ts">
-import singleEditor from 'src/components/CodeEditors/ApiEditor/v0/SingleRoute.vue'
+import {computed, ref, watch} from 'vue'
+import singleEditor from 'src/components/CodeEditors/ApiEditor/v0/SingleRoute.vue';
+
+// const routeAddition = ref({rname:'yoyou', })
+const allRoutes = ref([{rName:'Hello',content:'YO',contentType:'text',verb:'GET',statusCode:200},
+                      {rName:'/Keyal',content:'OKcat',contentType:'text',verb:'GET',statusCode:200}])
+const lRoutes = computed(()=>allRoutes.value.length)
+const lastRName = computed(()=>allRoutes.value[lRoutes.value-1].rName)
+watch(lastRName,()=>{
+
+  if(!!lastRName.value)
+    allRoutes.value.push({rName:'',content:'',contentType:'text',verb:'GET',statusCode:200})
+
+},{immediate: true})
+
 //TODO: optimize it as it is highly unoptimized sending all routes for every update
 function pushRoutes(){
   if(!navigator.serviceWorker.controller)
@@ -12,6 +27,6 @@ function pushRoutes(){
       console.error("this is wierd")
       return;
     }
-   navigator.serviceWorker.controller.postMessage({'dest': 'routes/loading',payaload:[{'hello':'bye'}]})
+   navigator.serviceWorker.controller.postMessage({'dest': 'routes/loading',payload:allRoutes.value})
 }
 </script>
--- src/components/CodeEditors/ApiEditor/v0/SingleRoute.vue
+++ src/components/CodeEditors/ApiEditor/v0/SingleRoute.vue
@@ -1,38 +1,71 @@
-<template>
-  <q-card class="q-pa-md">
-  <q-form>
-    <q-input name="route" prefix="/qRes/" filled label="Route" v-model="rname" ></q-input>
-    <section class="q-my-md">
-    <div class="bg-grey-2 q-pa-md flex">
-          <label for="minetype">Content Type:  </label>
-          <q-spacer></q-spacer>
-          <q-btn-toggle
-          id="mimetype"
-          v-model="model"
-          push
 
+
+
+<template>
+  <q-card class="q-pa-md shadow-2 q-y-md " >
+  <q-form>
+    <div class=" flex q-my-sm q-gutter-sm">
+      <q-select
+      label="Method"
+      outlined
+      :options="['GET','POST','PUT','PATCH','CREATE']" :modelValue="modelValue.verb"
+      @update:modelValue="$val=>emitUpdate('verb',$val)"
+      />
+      <q-input name="route"
+      prefix="/qRes/"
+      outlined
+      class="col-grow"
+      label="Route"
+      :modelValue="modelValue.rName"
+      @update:modelValue="$val=>emitUpdate('rName',$val)"/>
+
+    </div>
+    <div class="flex q-gutter-sm">
+          <q-select
+          class="col-4"
+          label="Content-Type"
+          id="mimetype"
+          :modelValue="modelValue.contentType"
+          @update:modelValue="$val=>emitUpdate('contentType',$val)"
+          outlined
           toggle-color="primary"
           :options="[
-            {label: 'Text', value: 'Text'},
-            {label: 'JSON', value: 'JSON'},
+            {label: 'Text', value: 'text'},
+            {label: 'JSON', value: 'json'},
           ]"
         />
+      </div>
+      <q-input
+      label="Content"
+      type="textarea"
+      :modelValue="modelValue.content"
 
-      </div >
-
-      <q-input label="content" filled></q-input>
-    </section>
-    <q-input type="number" v-model.number ="delay" label="Delay" suffix="ms" filled/>
-
+      @update:modelValue="$val=>emitUpdate('content',$val)"
+      outlined/>
 
   </q-form>
 </q-card>
 </template>
 
 <script setup>
-import {ref } from 'vue';
-const model = ref('JSON')
-const delay = ref(0)
-const rname = ref('JSON')
+const props = defineProps(['modelValue','rName'])
+const emit = defineEmits(['update:modelValue','update:rName'])
+//TODO: find better way to do it.
+//TODO: find better way to do it.
 
+function emitUpdate(scope,update){
+  const updatedValue = {...props.modelValue};
+  console.log(update)
+  console.log('HELLO',updatedValue)
+  updatedValue[scope] = update
+  console.log(updatedValue)
+  emit('update:modelValue',updatedValue)
+}
+
+
+// const updateSubProps = Object.keys(props.modelValue) //NOTE: model Value must have all keys present else this is HUGE BUG possiblity
+// for(let i = 0; i<updateSubProps; i++)
+// {
+
+// }
 </script>
